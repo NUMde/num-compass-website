e8c1ba9e45f9c271a93f4882835c260cd1ad7de4
Michael Stach <michael.stach@uni-ulm.de>
2021-03-15 19:47:33 +0100

    replace image with YouTube video in hero section

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,16 @@ $(window).on("load", function () {
 		$(this).remove();
 	});
 });
+
+(function ($) {
+    "use strict";
+
+    /* ========================================================================= */
+    /*	Portfolio Filtering Hook
+    /* =========================================================================  */
+    $('.play-icon i').click(function () {
+        var video = '<iframe allowfullscreen src="' + $(this).attr('data-video') + '"></iframe>';
+        $(this).replaceWith(video);
+    });
+
+})(jQuery);
